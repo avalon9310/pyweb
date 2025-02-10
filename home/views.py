42a4a53f80897c9fc84a52f7fe23a7d7ae06fc05
avalon <asd95027@gmail.com>
2025-02-10 20:04:38 +0800

    修改home

--- home/views.py
+++ home/views.py
@@ -1,13 +1,5 @@
 from django.shortcuts import render
+
 # Create your views here.
-
 def html(request):
-    return render(request, 'home.html')
-
-
-
-
-
-
-
-
+    return  render(request,"home.html")
--- pyweb/settings.py
+++ pyweb/settings.py
@@ -87,23 +87,23 @@ tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': tmpPostgres.path[1:],
-        'USER': tmpPostgres.username,
-        'PASSWORD': tmpPostgres.password,
-        'HOST': tmpPostgres.hostname,
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': tmpPostgres.path[1:],
+#         'USER': tmpPostgres.username,
+#         'PASSWORD': tmpPostgres.password,
+#         'HOST': tmpPostgres.hostname,
+#         'PORT': 5432,
+#     }
+# }
 
 
 # Password validation
