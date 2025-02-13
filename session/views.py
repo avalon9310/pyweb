1f28adfb2b2df0efd9d4babc796a45b45f6c11f6
avalon <asd95027@gmail.com>
2025-02-13 22:45:47 +0800

    登入功能製作

--- session/views.py
+++ session/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, redirect
-
+from django.http import  HttpResponse
 
 # Create your views here.
 def login(request):
-    pass
+    return render(request,'login.html')
 
 def logout(request):
     return  redirect("/")
 
 def login_process(request):
-    pass
+    # userAccount=request.GET["userAccount"]
+    # userPassword= request.GET["userPassword"]
+    userAccount = request.POST["userAccount"]
+    userPassword = request.POST["userPassword"]
+    print(userAccount,userPassword)
+    return HttpResponse()
 
 def check_session(request):
     pass
